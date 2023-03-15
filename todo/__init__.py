2769eddfdb5e331ba318cc311ef0e7613b0c7b50
Nardiéna Pratama <34739714+nardienapratama@users.noreply.github.com>
2023-03-15 03:57:59 +0000

    Edit json to not sort and add back square brackets

--- todo/__init__.py
+++ todo/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config['JSON_SORT_KEYS'] = False
 
     from .views.routes import api
     app.register_blueprint(api)
